27ed21ab990f3e2fc71cb50d004b4fe9a1d7e729
Евгений Дианов <dianov@MacBook-Pro.local>
2021-02-18 17:18:22 +1000

    added fonts converter

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,10 @@ const del = require("del");
 const sourcemaps = require("gulp-sourcemaps");
 const notify = require("gulp-notify");
 const fileinclude = require("gulp-file-include");
+const svgSprite = require("gulp-svg-sprite");
+const ttf2woff = require("gulp-ttf2woff");
+const ttf2woff2 = require("gulp-ttf2woff2");
+const ttf2eot = require("gulp-ttf2eot");
 
 // const webpack = require("webpack");
 // const webpackStream = require("webpack-stream");
@@ -21,19 +25,30 @@ function browsersync() {
   });
 }
 
+function fonts() {
+  src("./app/fonts/**.ttf").pipe(ttf2woff()).pipe(dest("./app/fonts"));
+  src("./app/fonts/**.ttf").pipe(ttf2eot()).pipe(dest("./app/fonts"));
+  return src("./app/fonts/**.ttf").pipe(ttf2woff2()).pipe(dest("./app/fonts"));
+}
+
+function fontsToDist() {
+  return src("./app/fonts/**/*").pipe(dest("dist/fonts"));
+}
+
 function clear() {
   return del("dist");
 }
 
 function html() {
-  return src(["./app/**/*.html"]).pipe(
-    fileinclude({
-      prefix: "@@",
-      basepath: "@file",
-    }))
-    .pipe(dest('./dist/'))
-    .pipe(browserSync.stream())
-  
+  return src(["./app/**/*.html"])
+    .pipe(
+      fileinclude({
+        prefix: "@@",
+        basepath: "@file",
+      })
+    )
+    .pipe(dest("./dist/"))
+    .pipe(browserSync.stream());
 }
 
 function img() {
@@ -80,7 +95,6 @@ function dist() {
   return src(
     [
       "app/css/style.min.css",
-      "app/fonts/**/*",
       "app/js/main.js",
       "app/**/*.html",
       "app/img/**/*",
@@ -94,17 +108,28 @@ function watching() {
   watch(["app/js/main.js"], js);
   watch(["app/**/*.html"], html);
   watch(["app/img/**/*"], img);
+  watch(["app/fonts/**/*.ttf"], fonts);
 }
 
-
-exports.html = html
+exports.html = html;
 exports.styles = styles;
 exports.js = js;
 exports.browsersync = browsersync;
+exports.fontsToDist = fontsToDist;
 exports.watching = watching;
 exports.img = img;
 exports.clear = clear;
 exports.dist = dist;
+exports.fonts = fonts;
 
 exports.build = series(clear, dist, img);
-exports.default = parallel(browsersync, html, styles, img, js, watching);
+exports.default = parallel(
+  browsersync,
+  fonts,
+  html,
+  styles,
+  img,
+  js,
+  fontsToDist,
+  watching
+);
